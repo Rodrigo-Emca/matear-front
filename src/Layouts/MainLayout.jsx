4a59fd63b948ef4e2bc2bf2148cc775850c5676e
Rodrigo <contactorodrigocabrera@gmail.com>
2023-04-10 11:35:38 -0300

    correciones

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from '../Pages/Header/Header'
 import { Outlet } from 'react-router-dom'
+import Footer from '../Components/Footer/Footer'
 
 export default function MainLayout() {
     return (
         <>
             <Header />
             <Outlet />
+            <Footer />
         </>
 
     )
