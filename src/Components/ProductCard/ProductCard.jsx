afd0e572e6ffbbdd69bb806a7bbf8fc43188c89d
Rodrigo <contactorodrigocabrera@gmail.com>
2023-04-10 18:45:01 -0300

    merge con Salo: correciones en css

--- src/Components/ProductCard/ProductCard.jsx
+++ src/Components/ProductCard/ProductCard.jsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import { Link as Anchor } from 'react-router-dom';
 import './productCard.css';
-import logoWhite from '../../Img/logo-white.png';
-import logoblack from '../../Img/logo-black.png';
 import CartButton from '../CartButton/CartButton';
-import favoriteIcon from '../../Img/corazon.png';
 
 export default function ProductCard(props) {
     const description =
@@ -29,7 +26,6 @@ export default function ProductCard(props) {
                 </div>
                 <div className="details">
                     <div className="center">
-                        {/* <img src={logoblack} alt="logo" className='logoMate'/> */}
                         <h1>{props.product_id.title}</h1>
                         {/* <h5>{description}</h5> */}
                         <p>$ {parseFloat(props.product_id.price).toLocaleString('es-AR', { minimumFractionDigits: 0, maximumFractionDigits: 3 })} ARS</p>
