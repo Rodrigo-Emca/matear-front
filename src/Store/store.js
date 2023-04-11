2a339936654d59946ae37b06115eb022fa8f3a33
salomon.cjl <contrerassalomon6@gmail.com>
2023-04-11 16:41:50 -0300

    action reducer - checks

--- src/Store/store.js
+++ src/Store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit'
 import productReducer from './ProductsAll/reducer'
 import getOneProductReducer from './ProductOne/reducers'
 import textReducer from './Search/reducer'
+import checksReducer from "./Checks/reducer"
 
 const store = configureStore({
     reducer: {
         productos: productReducer, //para traer TODOS los productos
         producto: getOneProductReducer, //para traer UN SOLO producto
         text: textReducer, //para filtrar por texto
+        checks: checksReducer, //para filtrar con checks
     }
 })
 export default store
