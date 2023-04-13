a9cbb684d5b74011445d2be76b4651f48a3faf0e
Rodrigo <contactorodrigocabrera@gmail.com>
2023-04-13 12:06:57 -0300

    Correccion en boton de agregar al carrito

--- src/Components/CartButton/CartButton.jsx
+++ src/Components/CartButton/CartButton.jsx
@@ -3,7 +3,6 @@ import './cartButton.css';
 import { BiCart } from "react-icons/bi"
 
 export default function CartButton(props) {
-    //console.log(props.product)
 
     const [pressed, setPressed] = useState(false);
 
@@ -14,35 +13,18 @@ export default function CartButton(props) {
     }, [props.product.idProduct]);
 
     const handleClick = () => {
-        const cartItems = JSON.parse(localStorage.getItem('cartItems'));
-        const existingKeys = Object.keys(localStorage).filter((key) =>
-            key.startsWith('cartItem')
-        );
-
-        if (cartItems) {
-            let itemExists = false;
-
-            existingKeys.forEach((key) => {
-                const item = JSON.parse(localStorage.getItem(key));
-                if (item.product.idProduct === props.product.idProduct) {
-                    localStorage.removeItem(key);
-                    setPressed(false);
-                    itemExists = true;
-                }
-            });
-
-            if (!itemExists) {
-                const cartItemCount = existingKeys.length + 1;
-                const newItemKey = `cartItem${props.product.idProduct}`;
-                localStorage.setItem(newItemKey, JSON.stringify(props));
-                setPressed(true);
-            }
+        const cartItemKey = `cartItem${props.product.idProduct}`;
+        const cartItemExists = localStorage.getItem(cartItemKey) !== null;
+    
+        if (cartItemExists) {
+            localStorage.removeItem(cartItemKey);
+            setPressed(false);
         } else {
-            const newItemKey = `cartItem${props.product.idProduct}`;
-            localStorage.setItem(newItemKey, JSON.stringify(props));
+            localStorage.setItem(cartItemKey, JSON.stringify(props));
             setPressed(true);
         }
     };
+    
 
     const buttonClass = `cart-button${pressed ? ' pressed' : ''}`;
 
